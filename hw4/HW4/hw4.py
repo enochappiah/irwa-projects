e7f255678ad32412a260396f15bb12f5ecfab207
eappiah7 <eappiah77@yahoo.com>
2024-04-09 01:24:51 -0400

    new and intermediate edits

--- hw4/HW4/hw4.py
+++ hw4/HW4/hw4.py
@@ -47,19 +47,24 @@ def get_nonlocal_links(url):
 
     #grab the domain of the url
     domain_name = urlparse(url).netloc
+    
+    
 
     for link, title in links:
+        #print("DOMAIN NAME:",domain_name)
+        #print("PARSE OBJECT: ",parse.urlparse(link).netloc)
         if not parse.urlparse(link).netloc == domain_name:
             filtered.append((link, title))
 
         #if link.startswith(url):
          #   continue
+            
 
 
     return filtered
 
 
-def crawl(root, wanted_content=[], within_domain=True):
+def crawl(root, within_domain, wanted_content):
     '''Crawl the url specified by `root`.
     `wanted_content` is a list of content types to crawl
     `within_domain` specifies whether the crawler should limit itself to the domain of `root`
@@ -75,11 +80,13 @@ def crawl(root, wanted_content=[], within_domain=True):
 
     #grab the domain of the url
     domain_name = urlparse(root).netloc
-
-    while not queue.empty():
+    i = 0
+    
+    while not queue.empty() and i < 50:
         url = queue.get()
+        i += 1
         #skip urls that are visited
-        if url in visited:
+        '''if url in visited:
             continue
         try:
             req = request.urlopen(url)
@@ -87,36 +94,73 @@ def crawl(root, wanted_content=[], within_domain=True):
 
             
            #grab current domain name
-            domain_name_curr = urlparse(url).netloc
+            domain_name_curr = parse.urlparse(url).netloc
 
             #grab content type from url
             content_type = req.headers['Content-Type']
-            if content_type not in wanted_content and url != root:
+            if wanted_content and url != root and content_type not in wanted_content:
                 continue
 
             #don't visit self-referenceing urls
-            if domain_name_curr.startswith(domain_name) and url != root:
+            #print(domain_name_curr, domain_name)
+            if domain_name_curr == domain_name and url != root: #TODO is this the right way to do this? Cannot NOT visit self-referencing urls and do within domain at the same time then
                 continue
             
-            #only visit urls within the domain
+            #only visit urls within the domain if within_domain is True and the url is not the root
             if within_domain:
-                if not domain_name_curr.startswith(domain_name) and url != root:
-                    continue
+                if url != root and not domain_name_curr.startswith(domain_name):
+                    continue'''
         
-            
-
-            visited.append(url)
-            visitlog.debug(url)
-
-            for ex in extract_information(url, html):
-                extracted.append(ex)
-                extractlog.debug(ex)
-
-            for link, title in parse_links(url, html):
-                queue.put(link)
-
+        visited.append(url)
+        visitlog.debug(url)
+        try:
+            req = request.urlopen(url)
+            html = req.read()
         except Exception as e:
             print(e, url)
+            continue
+
+        
+
+        for link, title in parse_links(url, html):
+                
+            if link in visited: 
+                continue
+            
+                
+            domain_name_curr = parse.urlparse(link).netloc
+            content_type = req.headers['Content-Type']
+            if wanted_content and url != root and content_type not in wanted_content:
+                continue
+                    #don't visit self-referenceing urls
+                    #print(domain_name_curr, domain_name)
+            '''if domain_name_curr == domain_name and url != root:
+                continue'''
+
+            if within_domain:
+                if url != root and not domain_name_curr == domain_name:
+                    continue
+                    
+                    
+                
+                            
+            for ex in extract_information(url, html):
+                extracted.append(ex)
+                extractlog.debug(ex)   
+            
+            
+
+            queue.put(link)
+
+        
+
+        '''except Exception as e:
+            print(e, url)
+            continue'''
+
+        
+        
+
 
     return visited, extracted
 
@@ -157,7 +201,9 @@ def main():
     nonlocal_links = get_nonlocal_links(site)
     writelines('nonlocal.txt', nonlocal_links)
 
-    visited, extracted = crawl(site, within_domain=False)
+    wanted_content = []
+    
+    visited, extracted = crawl(site, False, wanted_content)
     writelines('visited.txt', visited)
     writelines('extracted.txt', extracted)
 
